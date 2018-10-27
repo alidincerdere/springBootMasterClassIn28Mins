83093305a89db0ac37b4aede65e2cbf299b01a90
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-10-27 21:16:08 +0200

    pass GET request parameter to jsp. Use model addAttribute and ${...}

--- spring-boot-first-web-application/src/main/java/com/ali/springboot/web/springbootfirstwebapplication/Controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/ali/springboot/web/springbootfirstwebapplication/Controller/LoginController.java
@@ -3,13 +3,16 @@ package com.ali.springboot.web.springbootfirstwebapplication.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/login")
-    public String login(Model model) {
+    public String login(@RequestParam String name, Model model) {
+
+        model.addAttribute("name", name);
 
         return "login";
     }
