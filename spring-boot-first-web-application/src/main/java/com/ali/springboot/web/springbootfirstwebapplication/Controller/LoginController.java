eab9f765ecca5eebbabb4412a9ff6702b38c29e0
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-10-27 21:44:35 +0200

    using form and post methods

--- spring-boot-first-web-application/src/main/java/com/ali/springboot/web/springbootfirstwebapplication/Controller/LoginController.java
+++ spring-boot-first-web-application/src/main/java/com/ali/springboot/web/springbootfirstwebapplication/Controller/LoginController.java
@@ -1,20 +1,30 @@
 package com.ali.springboot.web.springbootfirstwebapplication.Controller;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String login(@RequestParam String name, Model model) {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage(Model model) {
+
+        //model.addAttribute("name", name);
+
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showWelcomePage(@RequestParam String name,  Model model) {
 
         model.addAttribute("name", name);
 
-        return "login";
+        return "welcome";
     }
 
 }
