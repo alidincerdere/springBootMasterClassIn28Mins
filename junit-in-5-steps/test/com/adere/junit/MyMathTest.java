4e8bff1e58445f8d32f9bed79937acc2c535367f
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-01 21:17:49 +0100

    junit @BeforeEach, @AfterEach annotations

--- junit-in-5-steps/test/com/adere/junit/MyMathTest.java
+++ junit-in-5-steps/test/com/adere/junit/MyMathTest.java
@@ -1,18 +1,31 @@
 package com.adere.junit;
 
 import static com.sun.tools.internal.ws.wsdl.parser.Util.fail;
+
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Created by adere on 1.11.2018.
  */
 public class MyMathTest {
 
+    @BeforeEach
+    public void before() {
+        System.out.println("before");
+    }
+
+
     @Test
     public void sum_with3numbers() {
 
+        System.out.println("test 1");
         int[] numbers = {1, 2, 3};
 
         MyMath myMath = new MyMath();
@@ -24,11 +37,19 @@ public class MyMathTest {
     @Test
     public void sum_with1number() {
 
+        System.out.println("test 2");
         int[] numbers = {3};
 
         MyMath myMath = new MyMath();
 
         assertEquals(3, myMath.sum(numbers));
 
+        assertTrue(true);
+
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("after");
     }
 }
