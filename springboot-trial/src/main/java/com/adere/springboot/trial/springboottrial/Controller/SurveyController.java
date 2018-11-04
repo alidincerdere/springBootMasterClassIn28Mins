0372f9e03deb14bf2c10c3109d1e364f8bd10ef4
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-04 20:45:17 +0100

    POST method, return with ResponseEntity.created (or noContent) and using @RequestBody

--- springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Controller/SurveyController.java
+++ springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Controller/SurveyController.java
@@ -3,11 +3,11 @@ package com.adere.springboot.trial.springboottrial.Controller;
 import com.adere.springboot.trial.springboottrial.Model.Question;
 import com.adere.springboot.trial.springboottrial.Service.SurveyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -30,6 +30,21 @@ public class SurveyController {
     @RequestMapping(value = "/surveys/{surveyId}/questions/{questionId}")
     public Question retriveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
 
-        return surveyService.retrieveQuestion(surveyId,questionId);
+        return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+    @RequestMapping(value = "/surveys/{surveyId}/questions", method = RequestMethod.POST)
+    public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+        if (question == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(question.getId()).toUri();
+
+
+        return ResponseEntity.created(location).build();
     }
 }
