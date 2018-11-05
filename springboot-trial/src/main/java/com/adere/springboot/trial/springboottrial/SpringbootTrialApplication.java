7831f311e1e87a7911df1f86d8aa858eac1c8180
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-05 17:09:31 +0100

    prod and dev profile properties files and @Property annotation

--- springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/SpringbootTrialApplication.java
+++ springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/SpringbootTrialApplication.java
@@ -2,6 +2,8 @@ package com.adere.springboot.trial.springboottrial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringbootTrialApplication {
@@ -9,5 +11,11 @@ public class SpringbootTrialApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringbootTrialApplication.class, args);
+	}
+
+	@Profile("prod")
+	@Bean
+	public String getDummy() {
+		return "dummy";
 	}
 }
