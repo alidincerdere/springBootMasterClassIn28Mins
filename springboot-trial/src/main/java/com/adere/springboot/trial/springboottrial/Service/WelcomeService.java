0974c7238e56fccb5f77f45be1d21d84597d2325
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-05 16:47:13 +0100

    @Value annotation

--- springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Service/WelcomeService.java
+++ springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Service/WelcomeService.java
@@ -1,5 +1,6 @@
 package com.adere.springboot.trial.springboottrial.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class WelcomeService {
 
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     public String retrieveWelcomeMessage() {
-        return "Good Morning";
+        return welcomeMessage;
     }
 }
