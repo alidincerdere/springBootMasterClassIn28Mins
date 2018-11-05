b05458ded319c3fd5fc075c6704f8e5010608545
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-05 23:09:42 +0100

    @ConfigurationProperties and dynamic configuration

--- springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Controller/WelcomeController.java
+++ springboot-trial/src/main/java/com/adere/springboot/trial/springboottrial/Controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.adere.springboot.trial.springboottrial.Controller;
 
+import com.adere.springboot.trial.springboottrial.Configuration.BasicConfiguration;
 import com.adere.springboot.trial.springboottrial.Service.WelcomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by adere on 2.11.2018.
@@ -14,8 +18,23 @@ public class WelcomeController {
     @Autowired
     WelcomeService welcomeService;
 
-    @RequestMapping(value = "welcome")
+    @Autowired
+    BasicConfiguration configuration;
+
+    @RequestMapping(value = "/welcome")
     public String welcomeHandler() {
         return welcomeService.retrieveWelcomeMessage();
+    }
+
+    @RequestMapping(value = "/dynamic-configuration")
+    public Map dynamicConfiguration() {
+
+        Map map = new HashMap();
+
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("key", configuration.isValue());
+
+        return map;
     }
 }
