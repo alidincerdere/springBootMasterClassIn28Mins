6ff1f69f5625afd3acb68190fc4540528235560a
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2018-11-07 16:36:16 +0100

    integration test for POST

--- springboot-trial/src/test/java/com/adere/springboot/trial/springboottrial/SurveyControllerIT.java
+++ springboot-trial/src/test/java/com/adere/springboot/trial/springboottrial/SurveyControllerIT.java
@@ -1,5 +1,6 @@
 package com.adere.springboot.trial.springboottrial;
 
+import com.adere.springboot.trial.springboottrial.Model.Question;
 import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,7 +26,28 @@ public class SurveyControllerIT {
 
     @LocalServerPort
     private int port;
-    
+
+    @Test
+    public void testAddQuestion() {
+
+        String url = "http://localhost:" + port + "/surveys/Survey1/questions/";
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        Question question = new Question("Question5",
+                "third largest english speaking country", "China", Arrays
+                .asList("India", "Russia", "United States", "China"));
+
+        HttpEntity entity = new HttpEntity<Question>(question, headers);
+        ResponseEntity<String>  response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+
+        assertTrue(actual.contains("/surveys/Survey1/questions/"));
+    }
+
 
     @Test
     public void test() throws JSONException {
